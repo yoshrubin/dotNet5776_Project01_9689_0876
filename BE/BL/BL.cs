7970ed078d9d36de23feb9cf79a82e6d3648e3b2
ShaulSolomon <Shaulsolomon@gmail.com>
2015-12-17 19:52:33 +0200

    working on getting BE to work

--- BE/BL/BL.cs
+++ BE/BL/BL.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DS;
 
 namespace BL
 {
-    public class Class1
+    public interface IBL
+    {
+        double SumMoneyDishes();
+    }
+    public class BL
     {
     }
 }
